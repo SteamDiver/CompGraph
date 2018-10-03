e28d1c9bde81d0e8abf29ffdb297f36914a70bb9
Дмитрий Донских <steamdiver@yandex.ru>
2018-10-04 00:29:34 +0300

    task 3 done

--- Task1/MainWindow.xaml.cs
+++ Task1/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Task1
@@ -23,6 +13,42 @@ namespace Task1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Canvas.Background = Brushes.Aquamarine;
+            var ellipse = new Ellipse()
+            {
+                Height = 100,
+                Width = 100,
+                Fill = Brushes.Yellow,
+                Margin = new Thickness(10, 10, 0, 0)
+            };
+
+            var text = new TextBlock()
+            {
+                Text = "Hello world!",
+                Margin = new Thickness(500, 100, 0, 0),
+                FontSize = 32,
+                FontWeight = FontWeights.DemiBold
+            };
+
+            var myPath = new Path
+            {
+                Stroke = Brushes.GreenYellow,
+                StrokeThickness = 4,
+                Data = Geometry.Parse(
+                    $"M 0,{(int) Canvas.ActualHeight} C 0,{(int) Canvas.ActualHeight} {(int) Canvas.ActualWidth / 2},170, {(int) Canvas.ActualWidth},170")
+            };
+            Canvas.Children.Add(ellipse);
+            Canvas.Children.Add(text);
+            Canvas.Children.Add(myPath);
         }
     }
 }
